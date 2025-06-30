f90c02e1735f3ae102633e141901e6406fd140fb
Yushi <charlizeyys@gmail.com>
2025-06-30 13:56:53 +0100

    Update day_5.py

--- Leetcode/day_5.py
+++ Leetcode/day_5.py
@@ -1,4 +1,4 @@
-# Best Time to Buy and Sell Stock
+# Best Time to Buy and Sell Stock I
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         if not prices or len(prices) == 1:
@@ -13,6 +13,26 @@ class Solution:
 
         return max_profit
 
+
+# Best Time to Buy and Sell Stock II 
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        # Intuition: 
+        # If tomorrow's price is higher than today's, buy today and sell tomorrow. 
+        # Repeat for every such pair, which effectively captures all upward trends.
+        if len(prices) == 1:
+            return 0
+        
+        total_profit = 0
+        for i in range(1, len(prices)): 
+            if prices[i] - prices[i-1] > 0:
+                total_profit += prices[i] - prices[i-1]
+
+        return total_profit
 
 
 # Longest Palindrome
