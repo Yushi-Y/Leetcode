e42aa7238be303de599c2567da8b372262c3658e
Yushi Yang <yushiyang@Yushis-MacBook-Air.local>
2026-01-13 10:56:52 +0000

    Updated DP

---
+++ Leetcode/DFS/[M] 131.Palindrome_Partitioning.py
@@ -0,0 +1,21 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        ans = []
+        path = []
+        n = len(s)
+
+        def dfs(i):
+            if i == n:
+                ans.append(path.copy())
+                return 
+
+            for j in range(i, n):
+                t = s[i:j+1]
+                if t == t[::-1]:
+                    path.append(t)
+                    dfs(j + 1)
+                    path.pop() # 撤销回溯选择，回到分支
+
+        dfs(0)
+        return ans
+        
---
+++ Leetcode/DFS/[M] 78.Subsets.py
@@ -0,0 +1,22 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        if n == 0:
+            return []
+        
+        ans = []
+        path = []
+
+        def dfs(i):
+            if i == n:
+                ans.append(path.copy())
+                return
+            
+            dfs(i + 1)
+            path.append(nums[i])
+            dfs(i + 1)
+            path.pop()
+
+        dfs(0)
+
+        return ans
